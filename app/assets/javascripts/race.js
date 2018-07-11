1ff242049488a519124581211e0374d47684e86b
raafa16 <raafa16@hotmail.com>
2018-07-12 02:34:05 +0600

    save dom objects in variables

--- app/assets/javascripts/race.js
+++ app/assets/javascripts/race.js
@@ -1,2 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function() {
+    var animation_id;
+
+    //saving dom objects to variables
+    var road = $('#road');
+    var car = $('#car');
+    var car_1 = $('#car_1');
+    var car_2 = $('#car_2');
+    var car_3 = $('#car_3');
+    var white_line_1 = $('#white_line_1');
+    var white_line_2 = $('#white_line_2');
+    var white_line_3 = $('#white_line_3');
+    var restart_section = $('#restart_section');
+    var restart_btn = $('#restart');
+    var score = $('#score');
+});
