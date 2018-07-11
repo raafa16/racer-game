56867bb30e4fbb27f9cb4d024432e690a5c8da86
raafa16 <raafa16@hotmail.com>
2018-07-12 02:42:54 +0600

    add car movement

--- app/assets/javascripts/race.js
+++ app/assets/javascripts/race.js
@@ -15,4 +15,80 @@ $(function() {
     var restart_section = $('#restart_section');
     var restart_btn = $('#restart');
     var score = $('#score');
+
+    //saving some initial setup
+    var road_width = parseInt(road.width());
+    var road_height = parseInt(road.height());
+    var car_width = parseInt(car.width());
+    var car_height = parseInt(car.height());
+
+    //some other declarations
+    var game_over = false;
+    var move_right = false;
+    var move_left = false;
+    var move_up = false;
+    var move_down = false;
+
+    /* Move the cars */
+    $(document).on('keydown', function(e) {
+        if (game_over === false) {
+            var key = e.keyCode;
+            if (key === 37 && move_left === false) {
+                move_left = requestAnimationFrame(left);
+            } else if (key === 39 && move_right === false) {
+                move_right = requestAnimationFrame(right);
+            } else if (key === 38 && move_up === false) {
+                move_up = requestAnimationFrame(up);
+            } else if (key === 40 && move_down === false) {
+                move_down = requestAnimationFrame(down);
+            }
+        }
+    });
+
+    $(document).on('keyup', function(e) {
+        if (game_over === false) {
+            var key = e.keyCode;
+            if (key === 37) {
+                cancelAnimationFrame(move_left);
+                move_left = false;
+            } else if (key === 39) {
+                cancelAnimationFrame(move_right);
+                move_right = false;
+            } else if (key === 38) {
+                cancelAnimationFrame(move_up);
+                move_up = false;
+            } else if (key === 40) {
+                cancelAnimationFrame(move_down);
+                move_down = false;
+            }
+        }
+    });
+
+    function left() {
+        if (game_over === false && parseInt(car.css('left')) > 0) {
+            car.css('left', parseInt(car.css('left')) - 5);
+            move_left = requestAnimationFrame(left);
+        }
+    }
+
+    function right() {
+        if (game_over === false && parseInt(car.css('left')) < road_width - car_width) {
+            car.css('left', parseInt(car.css('left')) + 5);
+            move_right = requestAnimationFrame(right);
+        }
+    }
+
+    function up() {
+        if (game_over === false && parseInt(car.css('top')) > 0) {
+            car.css('top', parseInt(car.css('top')) - 3);
+            move_up = requestAnimationFrame(up);
+        }
+    }
+
+    function down() {
+        if (game_over === false && parseInt(car.css('top')) < road_height - car_height) {
+            car.css('top', parseInt(car.css('top')) + 3);
+            move_down = requestAnimationFrame(down);
+        }
+    }
 });
